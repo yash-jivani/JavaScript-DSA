29138366a51c8221bc3b910b15f7b6131db29624
yash jivani <yashjivani17@gmail.com>
2022-06-13 19:52:54 +0530

    Insertion sort

---
+++ JS - DSA LvB/Sorting/03-insertion-sort.js
@@ -0,0 +1,55 @@
+/*
+
+insertion sort ::
+
+    Time complexity : 
+        Best case = O(N)  (if array is already sorted)
+        Worst case = O(N^2)
+    Space complexity = O(1)
+
+    Why intsertion sort : adaptive soring algo.
+
+    example : 
+
+     j i    (i=1,j=i-1)
+    [3,2,4,1]
+
+*/
+
+
+// with for loop
+function insertionSort(arr,n){
+    for(let i=1;i<n;i++){
+        let temp = arr[i]
+        let j = i-1;
+        for(;j>=0;j--){
+            if(temp<arr[j]){
+                // shift
+                arr[j+1] = arr[j];
+            }
+            else{
+                break;
+            }
+        }
+        arr[j+1] = temp;  
+    }
+    return arr;
+}
+console.log(insertionSort([24,18,38,43,14,40,1,54],8))
+
+// with while loop
+function insersionsort2(arr,n){
+    for(let i=1;i<n;i++){
+        let temp = arr[i];
+        let j = i-1;
+        while((arr[j]>temp) && (j>-1)){
+            // shift
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = temp;
+    }
+    return arr
+}
+
+console.log(insersionsort2([24,18,38,43,14,40,1,54],8));
