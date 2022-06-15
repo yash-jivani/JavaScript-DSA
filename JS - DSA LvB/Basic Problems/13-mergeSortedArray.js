0bf6ff2cb5062d2f2b470e247c9b2b3f75fdbd6a
yash jivani <yashjivani17@gmail.com>
2022-06-15 09:17:34 +0530

    merge 2 sorted array

--- JS - DSA LvB/Basic Problems/13-mergeSortedArray.js
+++ JS - DSA LvB/Basic Problems/13-mergeSortedArray.js
@@ -32,3 +32,45 @@ function merge(nums1,n,nums2,m){
   }
   
   console.log(merge([1,2,3,0,0,0],3,[2,5,6],3))
+
+
+// OR 
+
+function merge2(arr1,n,arr2,m){
+  let i=0;
+  let j=0;
+  var arr1_len = n
+  var arr2_len = m
+
+  let myArr = []
+  while(arr1_len && arr2_len){
+      if(arr1[i] <= arr2[j] ){
+          myArr.push(arr1[i]);
+          i++;
+          arr1_len--;
+      }
+      else{
+          myArr.push(arr2[j]);
+          j++;
+          arr2_len--;
+      }
+  }
+
+  while(arr1_len){
+      myArr.push(arr1[i]);
+      i++;
+      arr1_len--;
+  }
+
+  while(arr2_len){
+      myArr.push(arr2[j]);
+      j++;
+      arr2_len--;
+  }
+
+  return myArr;    
+
+}
+
+
+console.log(merge2([1,2,3,0,0,0],3,[2,5,6],3))
