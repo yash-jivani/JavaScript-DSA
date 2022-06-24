cdb72ac4a12d69021ae8cd32e841efc65bd5ff75
yash jivani <yashjivani17@gmail.com>
2022-06-24 09:02:05 +0530

    Dutch National Flag

--- JS - DSA LvB/Array/011-0s-1s-2s.js
+++ JS - DSA LvB/Array/011-0s-1s-2s.js
@@ -1,8 +1,40 @@
+// brute force | O(n logN)
+
 function zeros1s2s(arr){
     arr.sort(function (a,b) {return a-b});
     return arr;
 }
 
-// brute
-
 console.log(zeros1s2s([0,1,2,0,1,0,2,0,1]))
+
+// dutch national flag solution 
+
+function sort012(arr){
+    //your code here
+    let low = 0;
+    let high = arr.length-1;
+    let mid = low;
+    
+    while(mid<=high){
+        if(arr[mid] === 1 ){
+            mid++;
+        }
+        else if(arr[mid] === 2 ){
+            let tmp = arr[mid];
+            arr[mid] = arr[high];
+            arr[high] = tmp;
+            high--;
+        }
+        else if(arr[mid]===0){
+            let tmp = arr[mid];
+            arr[mid] = arr[low];
+            arr[low] = tmp;
+            
+            low++;
+            mid++;
+        }
+    }
+    return arr
+}
+
+console.log(sort012([0,1,2,0,1,0,2,0,1]))
