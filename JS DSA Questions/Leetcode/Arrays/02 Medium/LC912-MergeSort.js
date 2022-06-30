d0cbaadb496780ed96fb75bfe81d9c2ecf482365
yash jivani <yashjivani17@gmail.com>
2022-06-30 13:48:50 +0530

    LC-912

---
+++ JS DSA Questions/Leetcode/Arrays/02 Medium/LC912-MergeSort.js
@@ -0,0 +1,40 @@
+function merge(arr1, arr2) {
+    let i = 0;
+    let j = 0;
+    let myArr = [];
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] <= arr2[j]) {
+        myArr.push(arr1[i]);
+        i++;
+        } else {
+        myArr.push(arr2[j]);
+        j++;
+        }
+    }
+
+    while (i < arr1.length) {
+        myArr.push(arr1[i]);
+        i++;
+    }
+
+    while (j < arr2.length) {
+        myArr.push(arr2[j]);
+        j++;
+    }
+    return myArr;
+}
+
+var sortArray = function (nums) {
+    if (nums.length <= 1) {
+        return nums;
+    }
+
+    let mid = Math.floor(nums.length / 2);
+    let leftSide = sortArray(nums.slice(0, mid));
+    let rightSide = sortArray(nums.slice(mid));
+    return merge(leftSide, rightSide);
+};
+
+console.log(sortArray([2,1,3,5,4]))
+console.log(sortArray([2,1,3,5,4,22,12]))
+console.log(sortArray([2,1]))
