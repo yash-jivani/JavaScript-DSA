69a93676552ee1dfb26977450c556e43c836e9e3
yash jivani <yashjivani17@gmail.com>
2022-07-09 16:38:05 +0530

    Selection sort

---
+++ JS - DSA II/04 Sorting Algo/Selection-sort.js
@@ -0,0 +1,17 @@
+function selectionSort(arr){
+    for(let i=0;i<arr.length-1;i++){
+        let minIndex = i;
+        for(let j=i+1;j<arr.length;j++){
+            if(arr[minIndex]>arr[j]){
+                minIndex = j;
+            }
+        }
+        let tmp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = tmp;
+    }
+    return arr;
+}
+
+console.log(selectionSort([2,4,3,1]))
+console.log(selectionSort([2,4,3,1]))
