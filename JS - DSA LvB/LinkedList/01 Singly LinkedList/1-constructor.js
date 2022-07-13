ac6c22aa41eaa692f9d6a8ef84e527b64fe6f0b8
yash jivani <yashjivani17@gmail.com>
2022-07-13 19:13:20 +0530

    Singly LL Constructor

---
+++ JS - DSA LvB/LinkedList/01 Singly LinkedList/1-constructor.js
@@ -0,0 +1,18 @@
+class Node{
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class SinglyLL{
+    constructor(value){
+        const newNode = new Node(value);
+        this.head = newNode;
+        this.tail = newNode;
+        this.length = 1;
+    }
+}
+
+const linkedList = new SinglyLL(2);
+console.log(linkedList);
