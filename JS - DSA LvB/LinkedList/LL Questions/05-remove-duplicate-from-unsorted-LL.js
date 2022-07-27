f392f684760a12c81d6f59f997f2b136b9f30de9
yash jivani <yashjivani17@gmail.com>
2022-07-27 20:47:25 +0530

    Remove Duplicates (unsorted LL)

---
+++ JS - DSA LvB/LinkedList/LL Questions/05-remove-duplicate-from-unsorted-LL.js
@@ -0,0 +1,25 @@
+removeDuplicates(head)
+{
+    
+    let tmp = head;
+    let prev = head;
+    let myMap = {};
+    
+    while(tmp!==null){
+        if(myMap[tmp.data]){
+            
+            while(prev.next!==tmp){
+                prev = prev.next;
+            }
+            prev.next = tmp.next;
+            tmp = tmp.next;
+            
+        }else{
+            myMap[tmp.data] = true;
+            tmp = tmp.next;
+        }
+        
+    }
+    return head;
+
+}
