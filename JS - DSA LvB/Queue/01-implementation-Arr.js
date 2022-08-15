550ba0c2983d4eed56fe074a11daa9113b382c11
yash jivani <yashjivani17@gmail.com>
2022-08-15 16:15:20 +0530

    Implement Queue using Array

---
+++ JS - DSA LvB/Queue/01-implementation-Arr.js
@@ -0,0 +1,10 @@
+let myQueue = [];
+myQueue.push(1);
+myQueue.push(2);
+myQueue.push(3);
+myQueue.push(4);
+
+console.log(myQueue.shift());
+console.log(myQueue.shift());
+console.log(myQueue.shift());
+console.log(myQueue.shift());
