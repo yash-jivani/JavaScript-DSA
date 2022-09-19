da54e980ffe9d6b38db71481beca81178007b6fd
yash jivani <yashjivani17@gmail.com>
2022-09-19 11:10:39 +0530

    Optimize solution

--- JS - DSA LvB/Binary Tree/BST/01-pairSum.js
+++ JS - DSA LvB/Binary Tree/BST/01-pairSum.js
@@ -47,3 +47,79 @@ class Solution {
   		
   	}
 }
+
+// approach 2 : optimize solution  
+
+
+/*
+class Node
+{
+    constructor(x){
+        this.data=x;
+        this.left=null;
+        this.right=null;
+    }
+}
+*/
+
+/**
+ * @param {Node} root
+ * @param {number} target
+ * @return {number} 
+*/
+
+class BSTiterator{
+    myStack = [];
+    reverse = true;
+    
+    constructor(root,isReverse){
+        this.reverse  = isReverse;
+        this.pushAll(root);
+    }
+    
+    next(){
+        let tmpNode = this.myStack[this.myStack.length-1];
+        this.myStack.pop();
+        if(this.reverse === false){
+            this.pushAll(tmpNode.right);
+        }else{
+            this.pushAll(tmpNode.left);
+        }
+        return tmpNode.data;
+    }
+    
+    pushAll(node){
+        while(node!==null){
+            this.myStack.push(node);
+            if(this.reverse===true){
+                node = node.right;
+            }else{
+                node = node.left;
+            }
+        }
+    }
+}
+class Solution2 {
+  	isPairPresent(root,target){
+  		//code here
+  		if(root === null){
+  		    return  0;
+  		}
+  		let l = new BSTiterator(root,false);
+  		let r = new BSTiterator(root,true);
+  		
+  		let i = l.next();
+  		let j = r.next();
+  		while(i<j){
+  		    if(i+j === target){
+  		        return 1;
+  		    }else if((i+j) < target){
+  		        i = l.next();
+  		    }else{
+  		        j = r.next();
+  		    }
+  		}
+  		return 0;
+  		
+  	}
+}
