01dad41cb833bc41134284c3f4df00d5c6cf75b7
yash jivani <yashjivani17@gmail.com>
2022-10-18 10:12:15 +0530

    Implement using function

---
+++ JS - DSA LvB/Trie/01-implementation.js
@@ -0,0 +1,72 @@
+function Node() {
+  this.keys = new Map();
+  this.end = false;
+  this.setEnd = function () {
+    this.end = true;
+  };
+  this.isEnd = function () {
+    return this.end;
+  };
+}
+
+function Trie() {
+  this.root = new Node();
+
+  this.insert = function (input, node = this.root) {
+    if (input.length === 0) {
+      node.setEnd();
+      return;
+    } else if (!node.keys.has(input[0])) {
+      node.keys.set(input[0], new Node());
+      return this.insert(input.substr(1), node.keys.get(input[0]));
+    } else {
+      return this.insert(input.substr(1), node.keys.get(input[0]));
+    }
+  };
+
+  this.isWord = function (word) {
+    let node = this.root;
+    while (word.length > 1) {
+      if (!node.keys.has(word[0])) {
+        return false;
+      } else {
+        node = node.keys.get(word[0]);
+        word = word.substr(1);
+      }
+    }
+    return node.keys.has(word) && node.keys.get(word).isEnd() ? true : false;
+  };
+
+  this.print = function () {
+    let words = new Array();
+    let search = function (node, string) {
+      if (node.keys.size != 0) {
+        for (let letter of node.keys.keys()) {
+          search(node.keys.get(letter), string.concat(letter));
+        }
+        if (node.isEnd()) {
+          words.push(string);
+        }
+      } else {
+        string.length > 0 ? words.push(string) : undefined;
+        return;
+      }
+    };
+    search(this.root, new String());
+    return words.length > 0 ? words : mo;
+  };
+}
+
+myTrie = new Trie();
+myTrie.insert("ball");
+myTrie.insert("bat");
+myTrie.insert("doll");
+myTrie.insert("dork");
+myTrie.insert("do");
+myTrie.insert("dorm");
+myTrie.insert("send");
+myTrie.insert("sense");
+console.log(myTrie.isWord("doll"));
+console.log(myTrie.isWord("dor"));
+console.log(myTrie.isWord("dorf"));
+console.log(myTrie.print());
